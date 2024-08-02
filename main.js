b2b1040109ade09998c26337d187484e69bbcc8d
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-08-02 11:39:46 -0600

    Delete counter.js, add logic from webrtc, add steps md, add env example, modify index.html

--- main.js
+++ main.js
@@ -1,24 +1,86 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+/** Web RTC things  */
+const configuration = {
+  iceServers: [
+    {
+      urls: ['stun:stun.l.google.com:19302','stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
+    }
+  ],
+}
+const connectionWebRTC = new RTCPeerConnection(configuration)
+const channel = connectionWebRTC.createDataChannel('my-channel')
+channel.onmessage = (event) => {
+  console.log('Got a message', event.data)
+}
 
-setupCounter(document.querySelector('#counter'))
+channel.onopen = (event) = console.log('Connection opened')
+
+connectionWebRTC.onicecandidate = (event) => {
+  // offer = JSON.stringify(connectionWebRTC.localDescription)
+  //   console.log(`
+  //   New user
+  //   Offer: ${offer}
+  // `)
+
+  answer = JSON.stringify(connectionWebRTC.localDescription)
+  console.log(`
+  New user
+  Answer: ${answer}
+`)
+}
+
+connectionWebRTC.ondatachannel = (event) => {
+  // Creating prop my data channel where I will save the data channel gotten from the initiator of the negotiation
+  connectionWebRTC.myDataChannel = event.channel
+  connectionWebRTC.myDataChannel.onmessage = (event) => {
+    console.log(`New message from client:
+    ${event.data}
+    `)
+  }
+
+  connectionWebRTC.myDataChannel.onopen = (event) => {
+    console.log('connection opened')
+  }
+}
+
+// First we set the offer
+async function setOfferFromRemote(newOffer) {
+  // edit here and put the offer
+  try {
+    await connectionWebRTC.setRemoteDescription(newOffer)
+    console.log('connectionWebRTc', connectionWebRTC)
+    console.log('offer set')
+  } catch (error) {
+    console.log('Error while setting remot description', error)
+  }
+}
+
+// When the user wants to join, first we create an offer
+async function createOffer() {
+  const offer = await connectionWebRTC.createOffer()
+  console.log('offer', offer)
+  connectionWebRTC.setLocalDescription(offer)
+  console.log('offer set succesfully')
+}
+
+// Then we create an answer
+async function createAnswer() {
+  try {
+    const newAnswer = await connectionWebRTC.createAnswer()
+    connectionWebRTC.setLocalDescription(newAnswer)
+    console.log('answer created')
+  } catch (error) {
+    console.warn('error creating new answer', error)
+  }
+}
+
+async function setAnswer(answer) {
+  await connectionWebRTC.setRemoteDescription(answer)
+}
+
+document.querySelector('#create-session').addEventListener('click', async () => {
+  // Create an offer
+  await createOffer()
+  // Send the offer to the API
+})
---
+++ steps.md
@@ -0,0 +1,14 @@
+# Steps
+
+1. Create session
+   Create session with name: my-channel
+
+2. Create offer
+3. Send offer into the REST API and save it.
+
+4. Other user joins by clicking on join session
+   3.1 When the user join the session:
+   a. he sets the offer
+   b. he creates the answer
+   c. he sends the answer through the API
+5. Checks the channel for messages
